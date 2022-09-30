481d87ffb2acbc3c68d0ee68536d3b1bc5ec87a4
MilicaBl <93880377+MilicaBl@users.noreply.github.com>
2022-09-30 14:35:00 +0200

    created mobile tablet and desktop views

---
+++ main.js
@@ -0,0 +1,44 @@
+import "./styles/style.scss";
+let subscribeBtn = document.querySelector(".subscribeBtn");
+let unSubscribeBtn = document.querySelector(".unSubscribeBtn");
+let input = document.getElementById("input");
+let cookies = document.getElementById("cookies");
+let acceptBtn = document.getElementById("acceptBtn");
+let searchBtn = document.getElementById("searchBtn");
+let searchInput = document.getElementById("searchInput");
+
+acceptBtn.addEventListener("click", () => {
+  cookies.style.display = "none";
+});
+
+function disableBtns() {
+  if (!input.value) {
+    subscribeBtn.disabled = true;
+    unSubscribeBtn.disabled = true;
+  }
+  input.addEventListener("input", () => {
+    if (input.value.length > 5) {
+      subscribeBtn.disabled = false;
+      unSubscribeBtn.disabled = false;
+    } else if (input.value.length < 5) {
+      subscribeBtn.disabled = true;
+      unSubscribeBtn.disabled = true;
+    }
+  });
+}
+function disableSearchBtn() {
+  if (!searchInput.value) {
+    searchBtn.disabled = true;
+  }
+  searchInput.addEventListener("input", () => {
+    if (searchInput.value.length > 0) {
+      searchBtn.disabled = false;
+    } else if (searchInput.value.length == 0) {
+      searchBtn.disabled = true;
+    }
+  });
+}
+window.addEventListener("load", () => {
+  disableBtns();
+  disableSearchBtn();
+});
